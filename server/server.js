6d2b6ae30b60f075f1670bbac6eeec194e0816fb
agadzinski93 <agadzinski93@users.noreply.github.com>
2023-09-27 03:32:29 -0700

    Setup server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+var dotenv = require('dotenv');
+dotenv.config();
 const path = require('path');
 const PORT = 5000;
 
@@ -7,12 +9,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname,'frontend/dist')));
+if (process.env.NODE_ENV === 'production'){
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname,'frontend/dist')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
-});
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
+    });
+}else {
+    app.get("/",(req,res)=>{res.send("Server running")});
+}
 
 app.listen(()=>{
     console.log(`Server running on port ${PORT}`);
