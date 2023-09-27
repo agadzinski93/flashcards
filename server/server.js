b0d5186fd46754754122b6781b9124b148898859
agadzinski93 <agadzinski93@users.noreply.github.com>
2023-09-27 03:54:55 -0700

    Fixed Server.js

--- server/server.js
+++ server/server.js
@@ -11,10 +11,10 @@ app.use(express.urlencoded({extended:true}));
 
 if (process.env.NODE_ENV === 'production'){
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname,'frontend/dist')));
+    app.use(express.static(path.join(__dirname,'client/dist')));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
+        res.sendFile(path.resolve(__dirname,'client','dist','index.html'));
     });
 }else {
     app.get("/",(req,res)=>{res.send("Server running")});
