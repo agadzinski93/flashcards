6567cdad08fac7b387e8ef5f83e039dd72d91551
agadzinski93 <agadzinski93@users.noreply.github.com>
2023-09-26 21:18:26 -0700

    Initialize server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const path = require('path');
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname,'frontend/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'));
+});
+
+app.listen(()=>{
+    console.log(`Server running on port ${PORT}`);
+});
