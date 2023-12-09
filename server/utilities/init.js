72463617b2aae97834ad8b58bfb95afd7810a2c5
agadzinski93 <agadzinski93@users.noreply.github.com>
2023-12-08 23:59:35 -0800

    Backdated BCrypt and Fixed CSP

--- server/utilities/init.js
+++ server/utilities/init.js
@@ -22,7 +22,7 @@ const Routes =  {
               useDefaults:true,
               directives:{
                 imgSrc:["'self'"],
-                scriptSrc:["'self'","unsafe-inline"],
+                scriptSrc:["'self'","'unsafe-inline'"],
               }
             },
         }));
